7b7bcc01633ae3cb75d204c3826898bdd0ce3494
cat8dog <catherine.reyto@gmail.com>
2015-08-28 12:48:57 -0400

    Didn't work out :(

--- ToDo-Lynda/ToDo-Lynda/TableViewController.swift
+++ ToDo-Lynda/ToDo-Lynda/TableViewController.swift
@@ -1,15 +1,11 @@
-//
-//  TableViewController.swift
-//  ToDo-Lynda
-//
-//  Created by Catherine Reyto on 2015-08-27.
-//  Copyright (c) 2015 Catherine Reyto. All rights reserved.
-//
+
 
 import UIKit
 
+
 class TableViewController: UITableViewController {
     
+    @IBOutlet weak var Cell: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,24 +34,25 @@ class TableViewController: UITableViewController {
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return 0
+        return 1
     }
 
-    /*
+   
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell"), forIndexPath: indexPath) as! UITableViewCell
 
         // Configure the cell...
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
